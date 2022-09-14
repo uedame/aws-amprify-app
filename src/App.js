c30579f64bc7ba86d482dd605fa157b7b2295438
uedame <uedame0403@gmail.com>
2022-09-14 09:46:37 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello React app with AWS</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
